2463d9bfab28692d8b158aeb013d7cf78923ffa8
Ana Bulas Cruz <36916486+abulascruz@users.noreply.github.com>
2018-10-27 07:14:08 +0200

    Select delay data and send notifications to different topics.

--- disturbancedata.py
+++ disturbancedata.py
@@ -1,10 +1,34 @@
 import requests
-import datetime as dt
+import redis
+import pandas as pd
+from pyfcm import FCMNotification
+import os
 
-date_from = dt.date(2019, 10, 25)
-date_to = dt.date(2018, 10, 26)
+key_words = ['försenad', 'försening']
+transport_modes = 'bus,metro,train,ship,tram'
 base_url = "http://api.sl.se/api2/deviations.json"
-payload = {'key': 'd2b88c69767b471cb02b77d87c3d7fd6', 'TransportMode': 'bus,metro,train,ship,tram'}
-response = requests.get(base_url, params=payload)
 
-print(response.json())
+payload = {'key': os.getenv('TRAFIKLABAPIKEY'), 'TransportMode': transport_modes}
+
+response = requests.get(base_url, params=payload)
+data = response.json()['ResponseData']
+
+df_data = pd.DataFrame(data)
+
+r = redis.from_url(url=os.getenv('REDISCLOUD_URL'))
+
+df_data = df_data[
+    df_data.Details.apply(lambda detail: any(word in str.lower(detail) for word in key_words)) |
+    df_data.Header.apply(lambda header: any(word in str.lower(header) for word in key_words))]
+
+for case_id in df_data.DevCaseGid:
+    response = r.setnx(case_id, 'set')
+    if not response:
+        df_data = df_data[df_data.DevCaseGid != case_id]
+
+print(df_data)
+push_service = FCMNotification(api_key=os.getenv('FIREBASECLOUDSERVERAPI'))
+
+for index, row in df_data.iterrows():
+    result = push_service.notify_topic_subscribers(topic_name=df_data.Scope.at[index], message_body="test")
+    print('Successfully sent message:', result)
